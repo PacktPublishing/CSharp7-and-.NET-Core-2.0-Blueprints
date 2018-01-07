a3d3ce7bf110455b350a3eec9a1c6ae573398be1
Dirk Strauss <dirk.strauss@thisisembrace.com>
2018-01-07 17:26:38 +0200

    Added example to illustrate the difference between Encapsulation and Abstraction

---
+++ cricketScoreTrack/EncapsulationVsAbstraction/EncryptionHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationVsAbstraction
+{
+    public class EncryptionHelper
+    {
+        public string TextToEncrypt = "";
+        public void Encrypt()
+        {
+            string salt = GenerateSalt();
+            // Encrypt the text contained in the TextToEncrypt variable
+            SaveToDatabase();
+        }
+
+        private string GenerateSalt()
+        {
+            return "";
+        }           
+        
+        private void SaveToDatabase()
+        {
+
+        }
+    }
+}
---
+++ cricketScoreTrack/EncapsulationVsAbstraction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationVsAbstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EncryptionHelper encr = new EncryptionHelper();
+            encr.TextToEncrypt = "Secret Text";
+            encr.Encrypt();            
+        }
+    }
+}
