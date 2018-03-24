a2ffdbf62bb57cd1ab36147e94c8b75b00f3bdeb
Dirk Strauss <dirk.strauss@thisisembrace.com>
2018-03-24 11:15:01 +0200

    Folder rename
    Added chapter number

---
+++ Chapter 7 - Serverless/CoreMailValidation/Models/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreMailValidation.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ValidationMessage { get; set; } = "";
+    }
+}
--- Serverless/CoreMailValidation/Models/LoginModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CoreMailValidation.Models
-{
-    public class LoginModel
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string ValidationMessage { get; set; } = "";
-    }
-}
