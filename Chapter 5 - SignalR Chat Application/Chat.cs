4053d4b2a55f4e302891d7af983da8bb936e1382
Dirk Strauss <dirk.strauss@thisisembrace.com>
2018-03-24 11:08:18 +0200

    Folder rename
    Added chapter number

---
+++ Chapter 5 - SignalR Chat Application/Chat.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter5
+{
+    public class Chat:Hub
+    {
+        public Task Send(string sender, string message)
+        {
+            return Clients.All.InvokeAsync("UpdateChat", sender, message);
+        }
+
+        public Task ArchiveChat(string archivedBy, string path, string messages)
+        {
+            string fileName = "ChatArchive_" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm") + ".txt";
+            System.IO.File.WriteAllText(path + "\\" + fileName, messages);
+            return Clients.All.InvokeAsync("Archived", "Chat archived by "+ archivedBy);
+        }
+    }
+}
